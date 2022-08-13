1b04ee108bf7c65e7ddff43214a5630af8db5395
Philippe Augusto <philippeaugusto96@gmail.com>
2022-08-12 22:17:38 -0300

    Notas media
    testando commit

---
+++ notasmedia.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+n4 = float(input('Digite a quarta nota: '))
+M = ((n1 + n2 + n3 + n4)/4)
+
+print("A sua média: {}" .format(M))
